af4fa2ca0aac285a8f3ac7e1f8ecd7c22156e3a8
brysonsmith <smit4315@purdue.edu>
2023-01-22 22:57:51 -0500

    Initial Commit

---
+++ myStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myStory
+//
+//  Created by Bryson Smith on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
